c23fbfbefa635e3e930b7ba624e7993e4a9c607a
风格化法师 <2251770662@qq.com>
2022-02-22 17:14:32 +0800

    MaxProfit

---
+++ src/main/java/primary/array/MaxProfit.java
@@ -0,0 +1,50 @@
+package primary.array;
+
+
+/**
+ * 买卖股票问题,贪心思想.
+ * 给定一个数组 prices ，其中 prices[i] 表示股票第 i 天的价格。
+ * 输入: prices = [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3
+ */
+
+
+public class MaxProfit {
+    /**
+     * 挑战失败
+     * 官方解法1 贪心, 收集所有的上涨.
+     * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-ii-by-leetcode-s/
+     */
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; i++) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
+    }
+
+    /**
+     * 官方解法 2动态规划
+     */
+
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(new MaxProfit().maxProfit(prices));
+
+    }
+}
